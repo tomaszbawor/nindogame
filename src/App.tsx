4c70c06af1c431a6ef885bbb2090b4454ad3a9bd
Tomasz Bawor <bawortomasz@gmail.com>
2021-03-02 00:05:38 +0100

    Separate columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
-  return (
-    <div>Hello world</div>
-  );
+    return (
+      <>
+        <div className="container">
+          <div className="row">
+            <div className="col-2">Side</div>
+            <div className="col-8">Center</div>
+            <div className="col-2">Main</div>
+          </div>
+        </div>
+      </>
+
+    );
 }
 
 export default App;
