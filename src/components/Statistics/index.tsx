fe3623136d4fbf271e8231eadcb3a6de7d913057
Tomasz Bawor <bawortomasz@gmail.com>
2021-03-02 23:23:16 +0100

    Fixing scaling of an image on stats

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -4,10 +4,10 @@ import { PoolBar } from "../PoolBar";
 
 export const Statistics: React.FC<{}> = () => {
   return (
-    <div className="Statistics col-2">
+    <div className="col-2 Statistics ">
       <h1>Profile</h1>
       <div className="Statistics-Nickname">Nickname</div>
-      <img src="https://via.placeholder.com/150" alt="avatar" />
+      <img className="Statistics-Image" src="https://via.placeholder.com/150" alt="avatar" />
       <PoolBar poolType="Health" currentValue={70} maxValue={100} />
       <PoolBar poolType="Chakra" currentValue={50} maxValue={100} />
       <PoolBar poolType="Stamina" currentValue={30} maxValue={100} />
